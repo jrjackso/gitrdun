e5037e0f4e7dec5fbb4faca3c174bbc6a512c1c2
Joel Pipher <jpipher@tangamgaming.com>
2020-02-28 11:31:42 -0500

    Add post install scripts
    - install scripts can now be defined for a project and each of its
    dependencies

--- gitrdun.js
+++ gitrdun.js
@@ -3,7 +3,6 @@
 var exec = require('child_process').execSync;
 var fs = require('fs');
 var path = require('path');
-var rimraf = require('rimraf');
 var util = require('util');
 
 function Git() {
@@ -21,11 +20,6 @@ function Git() {
         var cmd = util.format('git -C %s fetch', dir);
         execute(cmd);
     };
-
-    function execute(cmd) {
-        console.log(cmd);
-        exec(cmd, { stdio: [0, 1, 2] });
-    }
 }
 
 function Installer(git) {
@@ -37,7 +31,13 @@ function Installer(git) {
             return;
         }
 
+        process.chdir(dir);
+
         var config = JSON.parse(fs.readFileSync(gitrdunJson));
+
+        if (config.install) {
+            execute(config.install);
+        }
 
         for (var repoName in config.dependencies) {
             var repoConfig = config.dependencies[repoName];
@@ -52,8 +52,10 @@ function Installer(git) {
 
             git.checkout(repoConfig.branch, repoDir);
 
-            if (repoConfig.paths) {
-                this.clean(repoDir, repoConfig.paths);
+            if (repoConfig.install) {
+                process.chdir(repoDir);
+                execute(repoConfig.install);
+                process.chdir(dir);
             }
 
             if (!repoConfig.shallow) {
@@ -61,45 +63,11 @@ function Installer(git) {
             }
         }
     };
+}
 
-    this.clean = function(baseDir, pathsToKeep) {
-        var pathsToKeepLowerCase = pathsToKeep.map(function(path) { return path.toLowerCase(); });
-        pathsToKeepLowerCase.push('.git');
-        var subPaths = fs.readdirSync(baseDir);
-
-        for (var i in subPaths) {
-            var subPath = subPaths[i].toLowerCase();
-            var fullPath = path.join(baseDir, subPath);
-            var isKeptPath = pathsToKeepLowerCase.indexOf(subPath.toLowerCase()) >= 0;
-
-            if (isKeptPath) {
-                continue;
-            }
-
-            var hasKeptSubPaths = isSubPathInList(subPath, pathsToKeepLowerCase);
-            var isDirectory = fs.lstatSync(fullPath).isDirectory();
-
-            if (!hasKeptSubPaths) {
-                rimraf.sync(fullPath);
-            }
-            else if (isDirectory) {
-                var subDir = path.join(baseDir, subPath);
-                this.clean(subDir, pathsToKeep);
-            }
-        }
-    };
-
-    var isSubPathInList = function(path, list) {
-        for (var i in list) {
-            var item = list[i];
-
-            if (item.indexOf(path.toLowerCase()) >= 0) {
-                return true;
-            }
-        }
-
-        return false;
-    };
+function execute(cmd) {
+    console.log(cmd);
+    exec(cmd, { stdio: [0, 1, 2] });
 }
 
 function main() {
